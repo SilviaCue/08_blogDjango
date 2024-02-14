82d670cf5fddbb4f00095d28615cd719ff5f98f3
SilviaCue <152323429+SilviaCue@users.noreply.github.com>
2024-02-14 15:17:29 +0100

    campos blog

--- blogg/models.py
+++ blogg/models.py
@@ -1,26 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Categoria(models.Model):
-    nombre=models.CharField(max_lenght=50)
+    nombre=models.CharField(max_length=50)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now_add=True)
     class Meta:
         verbose_name="categoria"
         verbose_name_plural="categoria"
-        def __str__(self):
-            return self.nombre
+    def __str__(self):
+        return self.nombre
     
     
 class Post(models.Model):
-    titulo=models.CharField(max_lenght=70)
-    contenido=models.CharField(max_lenght=200)
+    titulo=models.CharField(max_length=70)
+    contenido=models.CharField(max_length=200)
+    imagen=models.ImageField(upload_to="blogg", null=True, blank=True)
+    autor=models.ForeignKey(User, on_delete=models.CASCADE)
+    categorias=models.ManyToManyField(Categoria)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now_add=True)
     class Meta:
-        verbose_name="titulo"
-        verbose_name_plural="titulos"
-        def __str__(self):
-            return self.titulo
+        verbose_name="post"
+        verbose_name_plural="posts"
+    def __str__(self):
+        return self.titulo
     
     
