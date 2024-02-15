a6b12fe2d01fa7d9a5bb29daab4e417c5c80c50e
SilviaCue <152323429+SilviaCue@users.noreply.github.com>
2024-02-15 16:36:38 +0100

    corregir error imagenes

--- blog/settings.py
+++ blog/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,7 +80,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -119,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
--- blogg/models.py
+++ blogg/models.py
@@ -2,29 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Categoria(models.Model):
-    nombre=models.CharField(max_length=50)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now_add=True)
+    nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
     class Meta:
-        verbose_name="categoria"
-        verbose_name_plural="categoria"
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categoria'
+
     def __str__(self):
         return self.nombre
-    
-    
+
+
 class Post(models.Model):
-    titulo=models.CharField(max_length=70)
-    contenido=models.CharField(max_length=200)
-    imagen=models.ImageField(upload_to="blogg", null=True, blank=True)
-    autor=models.ForeignKey(User, on_delete=models.CASCADE)
-    categorias=models.ManyToManyField(Categoria)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now_add=True)
+    titulo = models.CharField(max_length=70)
+    contenido = models.CharField(max_length=200)
+    imagen = models.ImageField(upload_to="blogg", null=True, blank=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    categorias = models.ManyToManyField(Categoria)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
     class Meta:
-        verbose_name="post"
-        verbose_name_plural="posts"
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+
     def __str__(self):
         return self.titulo
-    
-    
