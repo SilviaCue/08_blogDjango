b818affc7807dade90f2ae37aa72df90279b43b6
SilviaCue <152323429+SilviaCue@users.noreply.github.com>
2024-02-16 13:01:01 +0100

    enlaces blog

--- blogg/urls.py
+++ blogg/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from . import views
 
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.home, name="Home"),
+    path('categoria/<int:categoria_id>', views.categoria, name="categoria"),
 
 ]
 
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogg/views.py
+++ blogg/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blogg.models import Post
+from blogg.models import Post, Categoria
 
 # Create your views here.
 
@@ -7,3 +7,18 @@ from blogg.models import Post
 def home(request):
     posts = Post.objects.all()
     return render(request, "blogg/home.html", {"posts": posts})
+
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    return render(request, "blogg/categoria.html", {"categoria": categoria, "posts": posts})
+
+
+"""def categoria(request, categoria_id):
+    print("Recibiendo solicitud para la categoría con ID:", categoria_id)
+    categoria = Categoria.objects.get(id=categoria_id)
+    print("Categoría encontrada:", categoria)
+    posts = Post.objects.filter(categorias=categoria)
+    print("Posts asociados:", posts)
+    return render(request, "blogg/categoria.html", {"categoria": categoria, "posts": posts})"""
